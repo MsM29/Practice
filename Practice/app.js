8a6c3df3955b8046b3e572acea2ff7acc370d35e
Krasnopolskij <krasnopolskiy.02@gmail.com>
2023-04-10 19:28:53 +0300

    Переделал авторизацию с json на mysql

--- Practice/app.js
+++ Practice/app.js
@@ -1,19 +1,25 @@
 const express = require('express');
-const fs = require('fs');
+const mysql = require('mysql');
 
 const port = 8080;
 const app = express(); // объект приложения
-let users; // список пользователей
 
 // запуск сервера
 function start_server() {
     try {
-        users = JSON.parse(fs.readFileSync('users/user_list.json', 'utf8'));
         app.listen(port, () => console.log(`server started at  http://localhost:${port}/login`));
     } catch (error) {
         console.log(error);
     }
 }
+
+// данные для подключения к mysql
+const pool = mysql.createPool({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'Ckj:ysqg@hjkm215',
+	database : 'node_auth'
+});
 
 // парсеры для данных
 app.use(express.json());
@@ -22,25 +28,27 @@ app.use(express.urlencoded({ extended: false }));
 // отправка статических данных
 app.use('/login', express.static(__dirname + '/public'));
 
-// обработка post
+// обработка post-запроса на авторизацию
 app.post('/login', function (request, response) {
     try {
-        // поиск по json
-        let login = request.body.login;
-        let password = request.body.password;
-        console.log(request.body)
+        const login = request.body.login;
+        const hash = request.body.password;
 
-        for (let i = 0; i < users.length; i++) {
-            const user = users[i];
-            if (user.login == login) {
-                if (user.hash == password) {
-                    return response.send('success_auth');
-                } else {
-                    return response.send('wrong_password');
-                }
+        pool.query('SELECT * FROM accounts WHERE login = ?', login,
+        function(error, results, fields) {
+			if (error) throw error;
+
+            // если results не содержит данных
+            if (results.length < 1) {
+                return response.send('user_not_found');
             }
-        }
-        return response.send('user_not_found');
+
+            if (results[0].hash == hash) {
+                return response.send('success_auth');
+            } else {
+                return response.send('wrong_password');
+            }
+		});
     } catch (error) {
         console.log(error);
     }
